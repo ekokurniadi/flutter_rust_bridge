e7e2c2a76cc3fd39472dba03bfdcbe6ba49855c4
Eko Kurniadi <eko.kurniadi@majoo.id>
2023-01-20 11:20:58 +0700

    update version

--- frb_example/pure_dart/dart/lib/ffi.io.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
-
-import 'bridge_generated.dart';
-
-FlutterRustBridgeExampleSingleBlockTestImpl initializeExternalLibrary(String path) =>
-    FlutterRustBridgeExampleSingleBlockTestImpl(
-      loadDylib(path),
-    );
